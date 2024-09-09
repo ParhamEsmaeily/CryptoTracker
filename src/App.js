8b258d53a379076daf9ab3376ba6ec90c52b5c77
Parham <parham.swe@gmail.com>
2024-09-08 20:01:32 -0400

    update transfering all local changes v1 to remote

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import Home from './pages/Home';
+import Top100 from './pages/Top100';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="min-h-screen bg-gray-100">
+        <Header />
+        <main className="py-8">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/top100" element={<Top100 />} />
+          </Routes>
+        </main>
+      </div>
+    </Router>
   );
 }
 
